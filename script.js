05ae0987b687dfccadc96199fac9d885f7add8b0
tomaSSurroca <tomassurroca@gmail.com>
2023-10-22 20:23:56 -0300

    agregar json

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Puedes agregar funcionalidades adicionales aquí si es necesario
+// funciones del carrito
 const carrito = [];
 const listaCarrito = document.getElementById('lista-carrito');
 const totalCarrito = document.getElementById('total-carrito');
@@ -31,4 +31,32 @@ function vaciarCarrito() {
     carrito.length = 0;
     mostrarCarrito();
 }
+
+
+// funciones del carrito (mantenlas como están)
+
+// Nueva función para cargar productos desde JSON
+function cargarProductosDesdeJSON() {
+    // Realizar una solicitud para cargar el archivo JSON
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "productos.json", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var productos = JSON.parse(xhr.responseText).productos;
+
+            // Aquí puedes agregar los productos al carrito si lo deseas
+            // Por ejemplo, puedes agregar todos los productos automáticamente al cargar la página
+            productos.forEach(function (producto) {
+                agregarAlCarrito(producto.nombre, producto.precio);
+            });
+        }
+    };
+    xhr.send();
+}
+
+// Ejecuta la función de carga cuando la página se cargue
+document.addEventListener("DOMContentLoaded", function () {
+    // Carga los productos desde el JSON
+    cargarProductosDesdeJSON();
+});
 
